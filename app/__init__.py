695062f496a1851d3bbea27480b7bdc0f2f694b6
Aayush Chaudhary <acaayush1111@gmail.com>
2024-04-12 10:18:13 +0530

    templates

--- app/__init__.py
+++ app/__init__.py
@@ -6,20 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return "perform login"
-    else:
-        return "return login form in html"
-    
-
-#uses get/post instead of "route"
-@app.get('/signup')
-def signup_get():
-    return "send the signup form"
-
-#sepration of concern, for cleanliness ig
-@app.post('/signup')
-def signup_post():
-    return "perform signup action in the backend"
+@app.get('/profile')
+def profile():
+    name = "demon"
+    age = 21
+    pets=['zo', 'azakaban', 'voldi', 'banana','pancake']
+    return render_template('profile.html', name=name, age=age, pets=pets )
