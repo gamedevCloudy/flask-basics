4a9fc06f6c83cbb9f7172c183021f8b91e922727
Aayush Chaudhary <acaayush1111@gmail.com>
2024-04-12 12:23:09 +0530

    uploading files and stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,46 @@
-from flask import Flask,request
-from flask import render_template,url_for, redirect
+import os
+from flask import Flask
+from flask import render_template,request,url_for, redirect, flash
+from werkzeug.utils import secure_filename
+
+
+
+
+UPLOAD_FOLDER = '$HOME/Downloads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.get('/profile')
-def profile():
-    name = "demon"
-    age = 21
-    pets=['zo', 'azakaban', 'voldi', 'banana','pancake']
-    return render_template('profile.html', name=name, age=age, pets=pets )
+@app.get('/submit')
+def submit_get(): 
+    return render_template('submit_file.html')
+
+@app.post('/submit')
+def submit_post(): 
+   # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('download_file', name=filename))
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
